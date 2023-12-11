d7e9347401d49150b25b54599d9751dbf32c5980
consuti-tec <consuti@consuti.com.br>
2023-12-11 10:38:50 -0300

    first commit

---
+++ src/AppConfig.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ColorTheme: "#5f99d4",
+  UrlPortal: "https://escoladoesporte.com.br/redesporti/unidades/secretaria-de-juventude-esporte-e-lazer",
+  UrlPainel: "https://painel.escoladoesporte.com.br/",
+  TimeSplashScreen: 3000,
+};
